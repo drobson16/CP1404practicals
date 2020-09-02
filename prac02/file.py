05bc0a7ec7e453737d59afdc368fe129b9cbc9d4
Davies Robson <davies.robson@my.jcu.edu.au>
2020-09-02 16:45:10 +1000

    Open and read the file then add numbers on the two lines in the file

--- prac02/file.py
+++ prac02/file.py
@@ -2,7 +2,11 @@
 
 
 user_name = input("Enter name: ")   # Gets input from user
-
 name_file = open("name.txt", "w")   # Opens file and write
-
 print("Your name is ", user_name, file=name_file)   # Prints string with user input to file
+
+open_file = open("numbers.txt", "r")
+num1 = int(open_file.readline())
+num2 = int(open_file.readline())
+open_file.close()
+print(num1 + num2)

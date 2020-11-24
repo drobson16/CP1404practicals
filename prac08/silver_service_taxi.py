b54a019092d094b262b1fc8ed00cc2c3ba7e92d0
Davies Robson <davies.robson@my.jcu.edu.au>
2020-11-24 21:30:26 +1000

    Taxi simulator

---
+++ prac08/silver_service_taxi.py
@@ -0,0 +1,21 @@
+"""
+SilverServiceTaxi class
+"""
+
+from prac08.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    flag_fall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(),
+                                                    self.flag_fall)
+
+    def get_fare(self):
+        return self.flag_fall + super().get_fare()
